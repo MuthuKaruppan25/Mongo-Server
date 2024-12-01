fcfe49ce61df2a88f9cd549f1282407ce6e750b3
MuthuKaruppan25 <muthukaruppanaakash76@gmail.com>
2024-12-01 17:06:48 +0530

    fourth

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config(); 
 const mongoose = require('mongoose');
 const dataRoutes  = require('./Routes/dataRoutes')
-
-const port = 8000;
+const port = process.env.PORT || 4000; // Use PORT from .env or default to 8000
+const mongoURI = process.env.MONGO_URI; // MongoDB URI from .env
+const frontendOrigin = process.env.FRONTEND_ORIGIN; 
 
 
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from your frontend
+  origin: frontendOrigin, // Allow requests from your frontend
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow these HTTP methods
   allowedHeaders: "Content-Type, Authorization", // Allow these headers
 };
@@ -18,7 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose
-  .connect("mongodb+srv://Gowtham:gowtham@bill.fx5nzqb.mongodb.net/?retryWrites=true&w=majority&appName=bill")
+  .connect(mongoURI)
   .then(() => {
     console.log("Server Connected");
   })
