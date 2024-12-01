e92a530c36d42a7d80ebe1961490cc095d012715
MuthuKaruppan25 <muthukaruppanaakash76@gmail.com>
2024-12-01 21:31:45 +0530

    ji

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const app = express();
 require("dotenv").config(); 
 const mongoose = require('mongoose');
 const dataRoutes  = require('./Routes/dataRoutes')
-const port = process.env.PORT || 4000; // Use PORT from .env or default to 8000
-const mongoURI = process.env.MONGO_URI; // MongoDB URI from .env
-const frontendOrigin = process.env.FRONTEND_ORIGIN; 
-
-
-
+const port = process.env.PORT || 4000; 
+const mongoURI = process.env.MONGO_URI; 
+const frontendOrigin = process.env.FRONTEND_ORIGIN;  
 const corsOptions = {
-  origin: ["https://famous-phoenix-ff4e15.netlify.app","http://localhost:3000"], // Allow requests from your frontend
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allow these HTTP methods
-  allowedHeaders: "Content-Type, Authorization", // Allow these headers
+  origin: ["https://famous-phoenix-ff4e15.netlify.app","http://localhost:3000"], 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
+  allowedHeaders: "Content-Type, Authorization", 
 };
 
 app.use(cors(corsOptions));
